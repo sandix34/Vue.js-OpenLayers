02fc417e32c881f25fb1807933d779e5c45b640b
sandrine <sandrinezocca@gmail.com>
2019-12-04 15:52:45 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <h1>Vuejs + OpenLayers</h1>
+    <FirstMap/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstMap from './components/FirstMap.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FirstMap
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/FirstMap.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id='map'></div>
+</template>
+
+<script>
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+export default {
+  mounted() {
+  new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({source: new OSM()})
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    })
+  }
+}
+
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+@import '~ol/ol.css';
+
+#map {
+  height: 80vh;
+  width: 80%;
+  margin: auto;
+}    
+</style>
