e74990777b5c3d8e5956c498171e469570640519
sandrine <sandrinezocca@gmail.com>
2019-12-06 11:41:29 +0100

    view a simple map

--- src/components/FirstMap.vue
+++ src/components/FirstMap.vue
@@ -3,14 +3,17 @@
 </template>
 
 <script>
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+// classes required to display the map
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
 
 export default {
-  mounted() {
-  new Map({
+  methods: {
+    initMap () {
+      // declare the map 
+      new Map({
       target: 'map',
       layers: [
         new TileLayer({source: new OSM()})
@@ -20,12 +23,15 @@ export default {
         zoom: 2
       })
     })
+    }
+  },
+  mounted() {
+    this.initMap()
   }
 }
 
 </script>
 
-<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 @import '~ol/ol.css';
 
